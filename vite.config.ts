e2853cd9e0e7c04f949b50aa8bb85e1adad48d6e
Alex <alex@lvira.dev>
2024-11-21 17:08:34 +0100

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/Lv_inbox/',
-    root: './'
+    base: '/Lv_inbox/'
 });
